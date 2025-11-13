dcba3d472850188f5c7aa5375ad6137098cecb63
RupertWatts <rw9g20@soton.ac.uk>
2025-11-13 00:31:22 +0000

    Update Figure 6.py

--- Figure 6.py
+++ Figure 6.py
@@ -1,44 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Parameters
-a=0
-b=1
-Nx=50
-delta_x=(b-a)/Nx #fixed
-x=np.linspace(a, b, Nx+1)
 
-t_final = 0.2#until shock
-delta_t_values = [0.01,0.02,0.03,0.05]
 
-#initial condition
-def f(x):
-    return np.exp(-(x-0.5)**2/(2*0.1**2))
 
-f=f(x)
+t_final = 0.2  # until shock
+delta_t_values = [0.01, 0.02, 0.03, 0.05]
 
-#upwind scheme up to a certain time step
-def inviscid_upwind(f, delta_t, Nx, delta_x, steps):
-    #initialising
-    u=f.copy()
-    u1=np.zeros(Nx+1)
-    for i in range (steps):
-        #finite difference method
-        for j in range(1,Nx+1):
-            u1[j]=u[j]-delta_t/(2*delta_x)*(u[j]**2-u[j-1]**2) 
-        u1[0] = u1[Nx]#periodic BCs to conserve mass
-        u[:] = u1 #for next time step
-    return u
-
-# Experiment with different dt
+# === Run for different Δt values ===
+plt.figure(figsize=(8, 5))
 
 for delta_t in delta_t_values:
-    steps = int(t_final/delta_t)
-    u = inviscid_upwind(f, delta_t, Nx, delta_x, steps)
-    plt.plot(x, u, label=f'$\Delta t$={delta_t}')
+    u = f(x)
+    steps = int(t_final / delta_t)
+
+    for _ in range(steps):
+        u = finite_difference_step(u, delta_x, delta_t)
+
+    plt.plot(x, u, label=f'Δt = {delta_t}')
 
 plt.legend()
-plt.xlabel('x')
-plt.ylim(0,1)
-plt.ylabel('u')
+plt.xlabel('x', fontsize=12)
+plt.ylabel('u(x, t)', fontsize=12)
+plt.ylim(0, 1)
+plt.tight_layout()
 plt.show()
+
+
